174a2639f8893e574c0582486fda6bff3bc0e6d1
Sofonias-23 <82233229+Sofonias-23@users.noreply.github.com>
2026-09-18 15:43:50 -0500

    Registrar navegación simplificada y pareja

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -14,22 +14,14 @@ export default function RootLayout() {
       >
         <Stack.Screen name="login" />
         <Stack.Screen name="index" />
-        <Stack.Screen
-          name="nuevo-gasto"
-          options={{ presentation: 'card' }}
-        />
-        <Stack.Screen
-          name="nuevo-ingreso"
-          options={{ presentation: 'card' }}
-        />
-        <Stack.Screen
-          name="categorias"
-          options={{ presentation: 'card' }}
-        />
-        <Stack.Screen
-          name="metodos-pago"
-          options={{ presentation: 'card' }}
-        />
+        <Stack.Screen name="movimientos" />
+        <Stack.Screen name="estadisticas" />
+        <Stack.Screen name="perfil" />
+        <Stack.Screen name="pareja" options={{ presentation: 'card' }} />
+        <Stack.Screen name="nuevo-gasto" options={{ presentation: 'card' }} />
+        <Stack.Screen name="nuevo-ingreso" options={{ presentation: 'card' }} />
+        <Stack.Screen name="categorias" options={{ presentation: 'card' }} />
+        <Stack.Screen name="metodos-pago" options={{ presentation: 'card' }} />
       </Stack>
     </FinanceProvider>
   );
