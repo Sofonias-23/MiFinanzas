915311d6d53d7edc99cb8053b92f189ec5944bd1
Sofonias-23 <82233229+Sofonias-23@users.noreply.github.com>
2026-09-18 20:38:41 -0500

    Registrar pantalla de presupuestos

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout() {
         <Stack.Screen name="pareja" options={{ presentation: 'card' }} />
         <Stack.Screen name="deudas" options={{ presentation: 'card' }} />
         <Stack.Screen name="saldar-deuda" options={{ presentation: 'card' }} />
+        <Stack.Screen name="presupuestos" options={{ presentation: 'card' }} />
         <Stack.Screen name="nuevo-gasto" options={{ presentation: 'card' }} />
         <Stack.Screen name="nuevo-ingreso" options={{ presentation: 'card' }} />
         <Stack.Screen name="categorias" options={{ presentation: 'card' }} />
