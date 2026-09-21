bbe358ab411258438a78a5bd6e2befc5b0e3c7b6
Sofonias-23 <82233229+Sofonias-23@users.noreply.github.com>
2026-09-21 16:35:11 -0500

    Corregir estilo absoluto para TypeScript

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -132,7 +132,11 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#07111F' },
   scenery: {
-    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
     overflow: 'hidden',
   },
   moon: {
